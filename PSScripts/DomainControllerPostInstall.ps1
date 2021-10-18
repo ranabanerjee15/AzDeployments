547bdc31532a38e821f0745eb43d2ae59d922269
Rana Banerjee <rana.b4523@gmail.com>
2021-10-18 19:20:31 +1000

    Added additional code

--- PSScripts/DomainControllerPostInstall.ps1
+++ PSScripts/DomainControllerPostInstall.ps1
@@ -12,3 +12,19 @@ choco install 7zip.install --y
 
 #Install Edge
 choco install microsoft-edge --y
+
+#install dot net framework  4.8
+choco install dotnetfx --y
+
+#Install URL Rewrite
+choco install urlrewrite --y
+
+#Download and Mount Exchange ISO Image
+$url = 'https://download.microsoft.com/download/5/3/e/53e75dbd-ca33-496a-bd23-1d861feaa02a/ExchangeServer2019-x64-CU11.ISO'
+$dst = 'C:\temp\Ex2019.ISO'
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($url, $dst)
+Mount-DiskImage $dst
+
+#Reboots Computer
+Restart-Computer -Force
