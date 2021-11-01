71af6142db27e92e29f748b72513a5a0afc3e40f
Rana Banerjee <rana.b4523@gmail.com>
2021-11-02 03:59:00 +1000

    added TLS1.2

--- PSScripts/Start-InstallPSModules.ps1
+++ PSScripts/Start-InstallPSModules.ps1
@@ -1,5 +1,9 @@
+#Set TLS to 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 #Install Modules
 Install-PackageProvider -Name NuGet -Scope AllUsers -Force -Confirm:$false -Verbose
+
 $modules = @(
     'ExchangeOnlineManagement'
     'ImportExcel'
